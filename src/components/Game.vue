42e6c947a074f188b8f12176a427ad02aed63475
Caillou <igorlacroute@gmail.com>
2018-05-12 20:39:56 +0200

    added fontawesome icons and improve game looks

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,20 +3,23 @@
     <div class="top-bar">
       <div class="dual-container">
         <fading-message duration="-1" v-bind:trigger="visibleElapsedTime">
-          <duration :amount="elapsedTime"></duration> wasted
+          <div class="bigger">Total time wasted</div>
+          <i class="fas fa-hourglass-end"></i>
+          <duration :amount="elapsedTime"></duration>
         </fading-message>
         <div class="levels">
-          <fading-message duration="-1" v-bind:trigger="visibleLevels">
+          <fading-message duration="-1" v-bind:trigger="visibleLevels" class="bigger">
             Level <span class="highlight">{{step+1}}</span>
           </fading-message>
           <fading-message duration="-1" v-bind:trigger="visibleNextDuration">
             <duration :amount="nextAmountToWait"></duration>
+            <i class="fas fa-hourglass-start"></i>
           </fading-message>
         </div>
       </div>
     </div>
     <div class="center-bar">
-      <div>
+      <div class="even-bigger">
         <span class="dots-compensator" v-if="visibleDots"></span>
         Wait for <duration :amount="cooldown"></duration>
         <animated-dots nbDots="4" v-if="visibleDots"></animated-dots>
@@ -50,10 +53,11 @@ export default {
       visibleDots: false,
       visibleNextDuration: false,
       steps: {
-        0: () => { this.visibleElapsedTime = true },
+        0: () => { this.visibleElapsedTime = true; this.taunt('Congrats ! You just waited 5 seconds') },
         1: () => { this.taunt('Got nothing else to do, eh ?') },
-        2: () => { this.visibleLevels = true; this.visibleNextDuration = true },
-        3: () => { this.visibleDots = true; this.taunt('Here, have some animated dots.') },
+        2: () => { this.visibleLevels = true; this.taunt('You\'re level 4, well played !') },
+        3: () => { this.visibleNextDuration = true; this.taunt('You can now see the duration of the next level') },
+        4: () => { this.visibleDots = true; this.taunt('Here, have some animated dots') },
         default: () => { this.taunt('LOL') }
       }
     }
@@ -116,7 +120,7 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.top-bar * {
+.top-bar div {
   margin-bottom: 5px;
 }
 .top-bar, .bottom-bar {
@@ -140,5 +144,17 @@ export default {
 .levels {
   text-align: right;
   white-space: nowrap;
+}
+.bigger {
+  font-size: 1.25em;
+}
+.even-bigger {
+  font-size: 1.5em;
+}
+.fa-hourglass-end {
+  margin-left: 10px;
+}
+.fa-hourglass-start {
+  margin-right: 10px;
 }
 </style>
