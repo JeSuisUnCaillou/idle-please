36148a9209255bb87fa81638089bc36dd9c36489
Caillou <igorlacroute@gmail.com>
2018-05-31 22:05:46 +0200

    add a step 25 about gorillas in step 23

--- src/components/steps.js
+++ src/components/steps.js
@@ -102,6 +102,9 @@ export default (game) => {
     },
     24: () => {
       game.taunt('Tell me you didn\'t jump', '', 'I really hope you did')
+    },
+    25: () => {
+      game.taunt('There was a gorilla', 'in the level 23', 'And you didn\'t see it')
     }
   }
 }
