d4402b90493cbed525c4f26554bcf2ecce40d72c
Caillou <igorlacroute@gmail.com>
2018-07-31 20:15:42 +0200

    finally fixed jumping shits

--- src/components/JumpingShit.vue
+++ src/components/JumpingShit.vue
@@ -1,20 +1,19 @@
 <template>
     <span class="jumping-shit highlight"
           :data-jumping="isJumping"
-          v-show="!isFinished"
           :style="{ transition: `transform ${duration}ms cubic-bezier(0,1,1,1.5), opacity ${duration}ms cubic-bezier(0,1,1,1.5)` }">
-      <span class="primary-text">+</span>1
+      <slot></slot>
     </span>
 </template>
 
 <script>
 export default {
   name: 'jumping-shit',
+  props: ['id'],
   data () {
     return {
       duration: 1000,
       isJumping: false,
-      isFinished: false,
       jumpingTimeout: null
     }
   },
@@ -24,8 +23,7 @@ export default {
         this.isJumping = true
         clearTimeout(this.jumpingTimeout)
         this.jumpingTimeout = setTimeout(() => {
-          this.isFinished = true
-          this.$emit('done')
+          this.$emit('done', this.id)
         }, this.duration)
       }, 0)
     }
--- src/components/JumpingShits.vue
+++ src/components/JumpingShits.vue
@@ -1,10 +1,11 @@
 <template>
   <span class="relative-container">
-    <jumping-shit v-for="(shit, index) in shits"
-                  @done="deleteShit(index)"
-                  :key="index"
-                  v-if="shit"
-                  :ref="`shit-${index}`">
+    <jumping-shit v-for="shit in shits"
+                  @done="deleteShit"
+                  :key="shit"
+                  :id="shit"
+                  :ref="`shit-${shit}`">
+      <span class="primary-text">+</span>1
     </jumping-shit>
   </span>
 </template>
@@ -16,21 +17,19 @@ export default {
   components: { JumpingShit },
   data () {
     return {
-      shits: {},
+      shits: [],
       shitId: 0
     }
   },
   methods: {
     addShit () {
       this.shitId += 1
-      this.$set(this.shits, this.shitId, true)
+      this.shits.push(this.shitId)
     },
-    deleteShit () {
-      // this.$set(this.shits, this.shitId, false)
-      delete this.shits[this.shitId]
+    deleteShit (id) {
+      this.shits.splice(this.shits.indexOf(id), 1)
     },
     jump () {
-      console.log('jump')
       this.addShit()
       setTimeout(() => {
         this.$refs[`shit-${this.shitId}`][0].jump()
